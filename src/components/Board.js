aa0b6919dedd135edaf51e74fb433bf5435586ed
mishal <mishaal79@gmail.com>
2022-01-30 20:32:15 +1100

    add time travel and move history to game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,50 +1,22 @@
 import React from 'react';
-import { calculateWinner, getPlayerSymbol } from '../helper';
 import { Square } from './Square';
 
 export class Board extends React.Component {
     constructor(props) {
         super(props);
         this.rowSize = 3;
-        this.hasWon = null;
-        this.state = {
-            squares: Array(9).fill(null),
-            turn: Boolean(true),
-            hasWon: null
-        };
     }
     renderSquare(i) {
         return (
-            <Square value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)} />
+            <Square value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)} />
         );
     }
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (this.hasWon || squares[i]) {
-            return null;
-        }
-        squares[i] = getPlayerSymbol(this.state.turn);
-        this.setState({
-            squares: squares,
-            turn: !this.state.turn
-        });
-    }
-
     render() {
-        this.hasWon = calculateWinner(this.state.squares);
-        let status;
-        if (this.hasWon) {
-            status = 'Player ' + (getPlayerSymbol(!this.state.turn)) + ' has won !';
-        }
-        else {
-            status = 'Next Player is ' + (this.state.turn ? 'X' : 'O');
-        }
 
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
--- src/index.js
+++ src/index.js
@@ -2,24 +2,86 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Board } from './components/Board';
+import { calculateWinner, getPlayerSymbol } from './helper';
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            history: [
+                {
+                    squares: Array(9).fill(null)
+                }
+            ],
+            turn: Boolean(true),
+            stepNumber: 0,
+        };
+
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            turn: (step % 2) === 0,
+        })
+
+    }
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = getPlayerSymbol(this.state.turn);
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            turn: !this.state.turn,
+            stepNumber: history.length
+        });
+
+
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber]
+        const hasWon = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to  move #' + move : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li >
+            );
+        });
+
+        let status;
+        if (hasWon) {
+            status = 'Player ' + (getPlayerSymbol(!this.state.turn)) + ' has won !';
+        }
+        else {
+            status = 'Next Player is ' + (this.state.turn ? 'X' : 'O');
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
+                <div className="history-buttons">
+
                 </div>
             </div>
         );
     }
 }
-
-// ========================================
 
 ReactDOM.render(
     <Game />,
